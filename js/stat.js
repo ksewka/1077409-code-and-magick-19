b5dda2f85e42cc7398a1d9b56a7c7f2e9e64e929
ksewka <54828553+ksewka@users.noreply.github.com>
2020-02-01 15:35:24 +1200

    добавила точку с запятой

--- js/stat.js
+++ js/stat.js
@@ -37,7 +37,7 @@ var getMaxElement = function (arr) {
     }
   }
   return maxElement;
-}
+};
 
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
