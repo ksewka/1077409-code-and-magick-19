002bcca4e1dfe43619acd8bd5131e346d6869c0d
ksewka <54828553+ksewka@users.noreply.github.com>
2020-02-02 00:07:06 +1200

    Ошибка с цветом плаща

---
+++ js/setup.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = [
+  'rgb (101, 137, 164)',
+  'rgb (241, 43, 107)',
+  'rgb (146, 100, 161)',
+  'rgb (56, 159, 117)',
+  'rgb (215, 210, 55)',
+  'rgb (0, 0, 0)'
+];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+// Функция для подбора случайного имени волшебника
+var getRandomName = function (arr1, arr2) {
+  var randomWizardName = Math.floor(Math.random() * arr1.length);
+  var randomWizardLastName = Math.floor(Math.random() * arr2.length);
+  var randomName = arr1[randomWizardName] + ' ' + arr2[randomWizardLastName];
+  return randomName;
+};
+
+// Функция для подбора случайного цвета мантии
+var getRandomCoatColor = function (arr) {
+  var randomCoatColor = Math.floor(Math.random() * arr.length);
+  return arr[randomCoatColor];
+};
+
+// Функция для подбора случайного цвета глаз
+var getRandomEyesColor = function (arr) {
+  var randomEyesColor = Math.floor(Math.random() * arr.length);
+  return arr[randomEyesColor];
+};
+
+var wizards = [
+  {
+    name: getRandomName(WIZARD_NAMES, WIZARD_LAST_NAMES),
+    coatColor: getRandomCoatColor(COAT_COLOR),
+    eyesColor: getRandomEyesColor(EYES_COLOR)
+  },
+  {
+    name: getRandomName(WIZARD_NAMES, WIZARD_LAST_NAMES),
+    coatColor: getRandomCoatColor(COAT_COLOR),
+    eyesColor: getRandomEyesColor(EYES_COLOR)
+  },
+  {
+    name: getRandomName(WIZARD_NAMES, WIZARD_LAST_NAMES),
+    coatColor: getRandomCoatColor(COAT_COLOR),
+    eyesColor: getRandomEyesColor(EYES_COLOR)
+  },
+  {
+    name: getRandomName(WIZARD_NAMES, WIZARD_LAST_NAMES),
+    coatColor: getRandomCoatColor(COAT_COLOR),
+    eyesColor: getRandomEyesColor(EYES_COLOR)
+  }
+];
+
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
