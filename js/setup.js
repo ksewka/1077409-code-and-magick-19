cbe6c44f78f35ff3337c519e4c42ba4b1c417d38
ksewka <54828553+ksewka@users.noreply.github.com>
2020-03-03 23:16:08 +1200

    загрузка и отправка данных на сервер при помощи AJAX

--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,7 @@
 
   var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var WIZARDS_AMOUNT = 4;
   var userDialog = document.querySelector('.setup');
   // userDialog.classList.remove('hidden');
 
@@ -10,6 +11,8 @@
   var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
+  var form = userDialog.querySelector('.setup-wizard-form');
+
 
   // Функция для подбора случайного имени волшебника
   var getRandomName = function (arr1, arr2) {
@@ -34,17 +37,46 @@
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
     wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
     return wizardElement;
   };
 
-  // Отрисовка волшебников на странице
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-  similarListElement.appendChild(fragment);
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  // Обработчик при успешной загрузке
+  var successHandler = function (someWizards) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < WIZARDS_AMOUNT; i++) {
+      fragment.appendChild(renderWizard(someWizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  // Обработчик при появлении ошибки
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  form.addEventListener('submit', function (evt) {
+    var submitButton = userDialog.querySelector('.setup-submit');
+    submitButton.textContent = 'Данные отправляются ...';
+    submitButton.disabled = true;
+    window.backend.save(new FormData(form), function () {
+      userDialog.classList.add('hidden');
+      submitButton.textContent = 'Сохранить';
+      submitButton.disabled = false;
+    });
+    evt.preventDefault();
+  });
+
+  window.backend.load(successHandler, errorHandler);
 })();
